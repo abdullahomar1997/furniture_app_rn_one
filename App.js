9b00544e311940e3f338067cc83b70bd18f0a0be
Avatar Tas <abdullahomar19971201@gmail.com>
2023-08-05 02:11:34 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useEffect, useState } from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import * as Font from 'expo-font';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// screens
+import { Home, ItemDetail } from "./screens/";
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    useEffect(() => {
+        // Load the custom font asynchronously
+        const loadFont = async () => {
+          await Font.loadAsync({
+            'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+            'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
+            'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+            // Add more custom fonts here if needed
+          });
+          setFontLoaded(true);
+        };
+    
+        loadFont();
+      }, []);
+
+      if (!fontLoaded) {
+        // Return a loading screen or null while the font is loading
+        return null;
+      }
+
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="ItemDetail" component={ItemDetail} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+
+export default () => {
+    return <App />;
+};
